1180a597014ede91e2ab2cb325f098bda2bdec16
Victor Ojewole <70405911+vojewole@users.noreply.github.com>
2022-03-21 17:30:23 -0700

    Add files via upload

---
+++ Singly Linked Lists/fronts.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class SLL {
+  constructor() {
+    this.head = null;
+  }
+
+  addFront = (value) => {
+    newNode = new Node(value);
+
+    if (this.head == null) {
+      this.head = newNode;
+      return this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      return this.head;
+    }
+  };
+
+  removeFront = () => {
+    if (this.head != null) {
+      this.head = newNode.next;
+      return this.head;
+    } else {
+      this.head = null;
+      return this.head;
+    }
+  };
+
+  front = (value) => {
+    newNode = new Node(value);
+
+    if (this.head == null) {
+      this.head = newNode;
+      return this.head.data;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      return this.head.data;
+    }
+  };
+}
